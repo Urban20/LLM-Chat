3c8476206dc7e5f00d9275fd0ba989afd58ecaf4
Urban20 <173964326+Urban20@users.noreply.github.com>
2026-07-01 00:35:35 -0300

    agrego separadores

--- utilidades/utilidades.go
+++ utilidades/utilidades.go
@@ -3,19 +3,34 @@ package utilidades
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/charmbracelet/glamour"
+	"golang.org/x/term"
 )
 
 var Memoria = []string{}
 
+func separador() {
+
+	x, _, _ := term.GetSize(int(os.Stdout.Fd()))
+	fmt.Println("\033[47m")
+	fmt.Println(strings.Repeat("_", x))
+	fmt.Println("\033[0m")
+
+}
+
 func Imprimir_markdown(txt string) error {
 
+	separador()
 	md, err := glamour.Render(txt, "dark")
 	if err != nil {
 		return err
 	}
 	fmt.Print(md)
+	separador()
+
 	return nil
 }
 
